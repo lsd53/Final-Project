4c5b0c77a36274e650050fc5ffc965409b253af7
Luis Gustavo De Medeiros <lsd53@corenll.edu>
2015-03-12 16:52:11 -0400

    Most recent changes to Part 1

--- moo.py
+++ moo.py
@@ -4,6 +4,8 @@ import sys
 import socket
 import random
 
+
+data_list=[]
 class MyUDPHandler(SocketServer.BaseRequestHandler):
     """
     This class works similar to the TCP handler class, except that
@@ -11,19 +13,35 @@ class MyUDPHandler(SocketServer.BaseRequestHandler):
     there is no connection the client address must be given explicitly
     when sending data back via sendto().
     """
-    data_list=[]
+    
     def handle(self):
         data = self.request[0].strip()
         socket = self.request[1]
-        print "{} wrote:".format(self.client_address[0])
-        self.data_list.append(data)
-        print self.data_list
-        socket.sendto(data.upper(), self.client_address)
+       	data_list.append(data)
+       	socket.sendto(data.upper(), self.client_address)
         
 
 
 def randguess():
 	return str(random.randint(0,9))+str(random.randint(0,9))+str(random.randint(0,9))+str(random.randint(0,9))
+
+def giveHint(guess, answer):
+        glist = list(str(guess))
+        alist = list(str(answer))
+        cows = 0
+        bulls = 0
+        for i in range(len(glist)):
+                if glist[i] == alist[i]:
+                        bulls += 1
+                elif glist[i] in alist:
+                        cows += 1
+        return (bulls, cows)
+
+
+
+
+
+
 
 HOST, PORT = "localhost", int(sys.argv[3])
 socket.setdefaulttimeout(1)
@@ -34,14 +52,67 @@ sock.sendto("Connection Established"+ "\n", (HOST, PORT))
 try: 
 	received = sock.recv(1024)
 	server = SocketServer.UDPServer((HOST, int(sys.argv[2])), MyUDPHandler)
-		#while True:
-		#server.handle_request()
+	i=0
+	while True:
+		numb=randguess()
+		guess="GUESS:"+numb
+		sock.sendto(guess,(HOST,int(sys.argv[3])))
+		print "I guessed with"+numb
+		while len(data_list)<i+1:
+			server.handle_request()
+		if data_list[i-1]==WIN:
+			print "I won!"
+			break
+		elif data_list[i-1]=="GUESS:"+sys.argv[1]:
+			sock.sendto("WIN",(HOST,int(sys.argv[3])))
+			break
+		else:
+			guess=data_list[i-1]
+			guess_numb=guess[5:]
+			s=giveHint(guess-numb,sys.argv[1])
+			sock.sendto(str(s[0])+"B"+str[1]+"C",(HOST,int(sys.argv[3])))
+		i=i+1
+		while len(data_list)<i+1:
+			server.handle_request()
+		i=i+1
+
+
+
+
 
 except socket.timeout:
 	server = SocketServer.UDPServer((HOST, int(sys.argv[2])), MyUDPHandler)
-	while True:
-
+	while len(data_list)<1:
 		server.handle_request()
+	i=1
+	print data_list[0]
+	
+	while True:
+		numb=randguess()
+		guess="GUESS:"+numb
+		sock.sendto(guess,(HOST,int(sys.argv[3])))
+		print "I guessed with"+numb
+		while len(data_list)<i+1:
+			server.handle_request()
+		if data_list[i-1]=="WIN":
+			print "I won!"
+			break
+		elif data_list[i-1]=="GUESS:"+sys.argv[1]:
+			sock.sendto("WIN",(HOST,int(sys.argv[3])))
+			break
+		else:
+			guess=data_list[i-1]
+			guess_numb=guess[5:]
+			s=giveHint(guess_numb,sys.argv[1])
+			sock.sendto(str(s[0])+"B"+str[1]+"C",(HOST,int(sys.argv[3])))
+		i=i+1
+		while len(data_list)<i+1:
+			server.handle_request()
+		i=i+1
+
+
+				
+
 		
     
 
